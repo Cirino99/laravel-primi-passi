700e7aaaefb213afe79647e77549bc85b41fade5
Simone <cirino.simone99@gmail.com>
2022-07-12 10:09:16 +0200

    aggiunte altre pagine

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -4,23 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Document</title>
 </head>
 <body>
     <nav>
         <ul>
             <li>
-                {{-- {{}} = <?php ?> --}}
                 <a href="{{ route('home') }}">Home</a>
             </li>
             <li>
-                <a href="{{ route('about') }}">About</a>
+                <a href="/about">About</a>
             </li>
             <li>
-                <a href="{{ route('store') }}">Store</a>
+                <a href="/store">Store</a>
             </li>
         </ul>
     </nav>
-    <h1>Hello Word</h1>
+    <h1>sono lo store</h1>
 </body>
 </html>
